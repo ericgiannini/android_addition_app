f9de87b1145aa4761498e11e9dc9e21d7c7cefd4
Floyd 2001 <floyd2001@Macintosh.attlocal.net>
2019-05-22 12:41:37 -0700

    additional files

---
+++ README.md
@@ -0,0 +1,1 @@
+# android_addition_app
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,12 +22,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        Button addButton = (Button)
+        // Creating instances of widgets, i.e., button, edittext, textview
+        var num1=findViewById<View>(R.id.firstNumEditText) as EditText
+        var num2=findViewById<View>(R.id.secondNumEditText) as EditText
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        var result=findViewById<View>(R.id.resultTextView) as TextView
+        var addition=findViewById<View>(R.id.addButton) as Button
+
+        addition.setOnClickListener(View.OnClickListener {
+            result.text="Add="+(num1.text.toString().toInt()+num2.text.toString().toInt()).toString()
+
+            Toast.makeText(this, result.text,Toast.LENGTH_LONG).show()
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
